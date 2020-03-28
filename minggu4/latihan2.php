fd2dc505ada777fef08df163ce71ccb10e33f239
unknown <pelolong24@gmail.com>
2020-03-28 20:06:12 +0700

    Tabel_Praktikum

--- minggu4/latihan2.php
+++ minggu4/latihan2.php
@@ -7,15 +7,24 @@
 <body>
     <h2>Membuat perulangan tabel</h2>
     <table border = "1" cellpadding = "10" cellspacing = "0">
+
         <?php
             for ( $b = 1; $b <= 10; $b++ ) { # $b = untuk baris
-                echo "<tr>";
+
+                    if ($b % 2 == 0){ // kondisi
+                        echo "<tr>";    // konidisi true
+                    }
+                    else {
+                        echo "<tr bgcolor = #F08080>"; // konddisi false
+                    }
+
                 for ( $k = 1; $k <= 8; $k++ ) { # $k = untuk kolom
                     echo "<td> $b , $k </td>";
                 }
                 echo "</tr>";
             }
         ?>
+
     </table>
 </body>
 </html>
