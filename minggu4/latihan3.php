5a222bba6b7b04e9e4fd8196bc4496572d752aca
unknown <pelolong24@gmail.com>
2020-03-28 14:32:55 +0700

    #TASK_8

---
+++ minggu4/latihan3.php
@@ -0,0 +1,11 @@
+<?php
+
+$x = 10;                #kondisi variabel
+
+    if ( $x < 20 ) {    #kondisi pengecekan dalam bracket
+        echo "benar";   #jika TRUE ini yang dicetak
+    }
+    else {
+        echo "salah";   #jika FALSE ini yang dicetak
+    }
+?> 
