a2eda1cf28d1731869bb2b22eede429231881cb6
unknown <pelolong24@gmail.com>
2020-03-28 13:34:43 +0700

    #TASK_3

--- minggu4/latihan.php
+++ minggu4/latihan.php
@@ -1,12 +1,13 @@
 <?php
+
 // perulangan
-// for
-echo "<H1> Menggunakan perulangan For <br> </H1>";
-    for ($i = 1; $i <= 5; $i++){
-        echo "Hai, saya urutan ke-$i <br>";
-}
-/* $i = 0; bagian inisialisai/ menentukan variable
-$i , 5;    bagian kondisi/ untuk memberhentikan kondisi
-           perulangan ketika berjumlah <5
-i++        bagian increment (menambah) kondisi */
+// while
+
+$i = 0; #bagian inisialisasi
+
+    while ($i < 5) { #didalam bracket() kondisi terminasi
+        echo "Hai, +62 people! <br>";
+        $i++; #increment
+    }
+
 ?>
