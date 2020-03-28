7dfeefc1240028c30f75e49f9455448cce72f021
unknown <pelolong24@gmail.com>
2020-03-28 14:27:14 +0700

    #TASK_7

--- minggu4/latihan2.php
+++ minggu4/latihan2.php
@@ -8,9 +8,9 @@
     <h2>Membuat perulangan tabel</h2>
     <table border = "1" cellpadding = "10" cellspacing = "0">
         <?php
-            for ( $b = 1; $b <= 3; $b++ ) { # $b = untuk baris
+            for ( $b = 1; $b <= 10; $b++ ) { # $b = untuk baris
                 echo "<tr>";
-                for ( $k = 1; $k <=5; $k++ ) {
+                for ( $k = 1; $k <= 8; $k++ ) { # $k = untuk kolom
                     echo "<td> $b , $k </td>";
                 }
                 echo "</tr>";
