ae9e9fce14ee7b0ef563946dde7f083d9d1bf3ef
unknown <pelolong24@gmail.com>
2020-03-28 12:44:41 +0700

    Sturktur Control

---
+++ minggu4/latihan.php
@@ -0,0 +1,12 @@
+<?php;
+// perulangan
+// for
+
+for ($i = 0; $i < 5; i++){
+        echo "Hello Lord ! <br>";
+}
+/* $i = 0; bagian inisialisai/ menentukan variable
+$i , 5;    bagian kondisi/ untuk memberhentikan kondisi
+           perulangan ketika berjumlah <5
+i++        bagian increment (menambah) kondisi */
+?>
