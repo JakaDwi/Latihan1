42ce24cecc67b29bbe72ce8c68656e64a7c5f19a
unknown <pelolong24@gmail.com>
2020-04-10 15:03:01 +0700

    Materi Function

---
+++ minggu6/date.php
@@ -0,0 +1,12 @@
+<?php
+    //date untuk menampilkan tanggal dengan format tertentu
+
+    echo date ("l d, m M Y");
+
+    /* date ("l") untuk menampilkan nama hari
+       date(“d”)  untuk menampilkan tanggal
+       date(“M”)  untuk menampilkan bulan dalam Text
+       date(“m”)  untuk menampilkan bulan dalam angka
+       date(“Y”)  untuk menampilkan Tahun */
+    
+    ?>
