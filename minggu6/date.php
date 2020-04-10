20f429f2aea30831c35ce64bc054389128669d50
unknown <pelolong24@gmail.com>
2020-04-10 16:02:50 +0700

    #TASK_4

--- minggu6/date.php
+++ minggu6/date.php
@@ -4,7 +4,7 @@
     echo date (
         "l, d-M-Y",
         time() +
-            60 * 60 * 24 * 1000
+            60 * 60 * 24 - 500
             );
 
     // dengan format tersebut kita dapat mengetahui 100 hari yang akan datang hari apa
