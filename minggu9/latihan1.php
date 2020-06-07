dcd26ce4f67b2379aea3cdb55f28936587e3dff8
unknown <pelolong24@gmail.com>
2020-06-07 20:13:54 +0700

    GET

--- minggu9/latihan1.php
+++ minggu9/latihan1.php
@@ -1,13 +1,68 @@
 <?php
-// Lingkup variabel
 
-$angka = 10;
-function Angka ()
-{
-    $angka = 20;
-    echo $angka;
-}
-    echo $angka;
-    echo "<br>";
-    Angka();
+$koleksi = [
+    [
+        "nama" => "Peacock Bass Monoculus", 
+        "asal" => "Sungai Amazon", 
+        "jenis" => "Predator Karnivora Cenderung Omnivora",
+        "gambar"=> "mono.png"
+    ],
+    
+    [
+        "nama" => "Peacock Bass Orinoco", 
+        "asal" => "Sungai Amazon", 
+        "jenis" => "Predator Karnivora Cenderung Omnivora",
+        "gambar"=> "orino.png"
+    ],
+
+    [
+        "nama" => "Payara", 
+        "asal" => "Amerika Selatan", 
+        "jenis" => "Predator Karnivora Cenderung Omnivora",
+        "gambar"=> "payara.png"
+    ],
+
+    [
+        "nama" => "Tiger Shovelnose Catfish", 
+        "asal" => "Suriname", 
+        "jenis" => "Predator Karnivora Cenderung Omnivora",
+        "gambar"=> "tiger.png"
+    ],
+
+    [
+        "nama" => "Red Tail Catfish", 
+        "asal" => "Sungai Amazon", 
+        "jenis" => "Predator Karnivora Cenderung Omnivora",
+        "gambar"=> "rtc.png"
+    ],
+
+    [
+        "nama" => "Dazt Borneo", 
+        "asal" => "Perairan Borneo", 
+        "jenis" => "Predator Karnivora Cenderung Omnivora",
+        "gambar"=> "dazt.png"
+    ]
+
+ ];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan GET</title>
+</head>
+<body>
+    <h1>Daftar Koleksi Ikan Predator Saya</h1>
+        <ul>
+            <?php foreach ($koleksi as $klk) : ?>
+                <li>
+                    <a 
+                        href="latihan2.php?nama=<?= $klk["nama"];?>&asal=<?= $klk["asal"];?>&jenis=<?= $klk["jenis"];?>&gambar=<?= $klk["gambar"];?>"><?= $klk["nama"];?>
+                    </a>
+                </li>
+                
+            <?php endforeach; ?>
+        </ul>
+</body>
+</html>
---
+++ minggu9/latihan2.php
@@ -0,0 +1,27 @@
+<?php
+
+if(!isset($_GET["nama"])){
+    //redirect
+    header("Location: latihan1.php");
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Koleksi</title>
+</head>
+<body>
+    <ul>
+        <li><img src="img/<?= $_GET["gambar"]?>"></li>
+        <li><?= $_GET["nama"]; ?></li>
+        <li><?= $_GET["asal"]; ?></li>
+        <li><?= $_GET["jenis"]; ?></li>
+    </ul>
+
+    <a href="latihan1.php">Kembali ke halaman utama</a>
+</body>
+</html>
